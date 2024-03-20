cab4c380188771c3701b0b46463e948db7fc210a
ak0327 <ca.fie.0220@gmail.com>
2024-03-20 23:48:59 +0900

    :bug: hugo: fixed path for https access

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -15,7 +15,7 @@ openssl req \
         -newkey rsa:2048 \
         -out /etc/nginx/ssl/inception.crt \
         -keyout /etc/nginx/ssl/inception.key \
-        -subj "/C=JP/ST=Tokyo/L=city/O=42Tokyo/CN=takira.42.fr"
+        -subj "/C=JP/ST=Tokyo/L=city/O=42Tokyo/CN=takira.42.fr" > /dev/null
 
 openssl req \
         -x509 \
@@ -23,11 +23,56 @@ openssl req \
         -newkey rsa:2048 \
         -out /etc/nginx/ssl/inception_hugo.crt \
         -keyout /etc/nginx/ssl/inception_hugo.key \
-        -subj "/C=JP/ST=Tokyo/L=city/O=42Tokyo/CN=takira.hugo.com"
+        -subj "/C=JP/ST=Tokyo/L=city/O=42Tokyo/CN=takira.hugo.com" > /dev/null
 
 exec "$@"
 
+## CA
 #mkdir demoCA
 #echo "01" > demoCA/serial
 #echo "00" > demoCA/crlnumber
 #touch demoCA/index.txt
+#
+#CERT_CA_NAME=ca
+#openssl genrsa 2048 > $CERT_CA_NAME.key
+#
+#openssl req \
+#        -new \
+#        -subj "/C=JP/ST=Tokyo/O=$CERT_CA_NAME/CN=$CERT_CA_NAME" \
+#        -key $CERT_CA_NAME.key \
+#        -out $CERT_CA_NAME.csr
+#
+#openssl ca \
+#        -selfsign \
+#        -batch \
+#        -days 3650 \
+#        -extensions v3_ca \
+#        -outdir . \
+#        -keyfile $CERT_CA_NAME.key \
+#        -in $CERT_CA_NAME.csr \
+#        -out $CERT_CA_NAME.crt
+#
+## Server
+#CERT_NAME=cert
+#openssl genrsa 2048 > $CERT_NAME.key
+#
+#SUBJECT_ALT="subjectAltName = DNS:localhost"
+#openssl req \
+#        -new \
+#        -x509 \
+#        -subj "/C=JP/ST=Tokyo/O=$CERT_CA_NAME/CN=$CERT_NAME" \
+#        -extensions v3_req \
+#        -addext "$SUBJECT_ALT" \
+#        -key $CERT_NAME.key \
+#        -out $CERT_NAME.csr
+#
+#openssl x509 \
+#        -text \
+#        -days 365 \
+#        -CA $CERT_CA_NAME.crt \
+#        -CAkey $CERT_CA_NAME.key \
+#        -in $CERT_NAME.csr \
+#        -out $CERT_NAME.crt
+# https://qiita.com/betarium/items/2b396953543331545a44
+
+
