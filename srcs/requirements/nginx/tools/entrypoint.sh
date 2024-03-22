326be6584c9aa1daede193384139adfb87674b85
ak0327 <ca.fie.0220@gmail.com>
2024-03-22 21:05:32 +0900

    :recycle: refactor: nginx

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,30 +1,37 @@
 #!/bin/sh
 
-mkdir -p ${CRT_DIR}
+set -eu -o pipefail
 
-# ssl certificate
-## wordpress page
-openssl req \
-        -x509 \
-        -noenc \
-        -newkey rsa:${RSA_KEY_BITS} \
-        -out ${CRT_DIR}/inception.crt \
-        -keyout ${CRT_DIR}/inception.key \
-        -subj "/C=${COUNTRY}/ST=${STATE}/O=${ORGANIZATION}/CN=${WP_DOMAIN}" > /dev/null 2>&1
+make_ssl_cert() {
+  mkdir -p ${CRT_DIR}
 
-## hugo page
-openssl req \
-        -x509 \
-        -noenc \
-        -newkey rsa:${RSA_KEY_BITS} \
-        -out ${CRT_DIR}/inception_hugo.crt \
-        -keyout ${CRT_DIR}/inception_hugo.key \
-        -subj "/C=${COUNTRY}/ST=${STATE}/O=${ORGANIZATION}/CN=${HUGO_DOMAIN}" > /dev/null 2>&1
+  ## wordpress page
+  openssl req \
+          -x509 \
+          -noenc \
+          -newkey rsa:${RSA_KEY_BITS} \
+          -out ${CRT_DIR}/inception.crt \
+          -keyout ${CRT_DIR}/inception.key \
+          -subj "/C=${COUNTRY}/ST=${STATE}/O=${ORGANIZATION}/CN=${WP_DOMAIN}" > /dev/null 2>&1
+
+  ## hugo page
+  openssl req \
+          -x509 \
+          -noenc \
+          -newkey rsa:${RSA_KEY_BITS} \
+          -out ${CRT_DIR}/inception_hugo.crt \
+          -keyout ${CRT_DIR}/inception_hugo.key \
+          -subj "/C=${COUNTRY}/ST=${STATE}/O=${ORGANIZATION}/CN=${HUGO_DOMAIN}" > /dev/null 2>&1
+}
 
 
-# conf
-envsubst '${WP_DOMAIN},${HUGO_DOMAIN},${CRT_DIR}' \
-          < /etc/nginx/http.d/default.conf.template \
-          > /etc/nginx/http.d/default.conf
+make_conf() {
+  envsubst '${WP_DOMAIN},${HUGO_DOMAIN},${CRT_DIR}' \
+            < /etc/nginx/http.d/default.conf.template \
+            > /etc/nginx/http.d/default.conf
+}
 
+
+make_ssl_cert
+make_conf
 exec "$@"
